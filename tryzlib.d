8ce935f3b2da6af001d6b22384837d5e168c5b44
thotgamma <gamma@e.thotgamma.com>
2016-02-09 14:39:46 +0900

    他のPDF観察用にデコードするプログラムを書いた
    後々GTeXTにはstreamをエンコードするようにするからその前準備というか練習というか

---
+++ tryzlib.d
@@ -0,0 +1,35 @@
+import std.stdio;
+import std.zlib;
+import std.conv;
+
+void main(){
+	string line;
+	bool instream;
+	string buff;
+	string[] streams;
+
+	auto fin = File("import.pdf","r");
+	while(!fin.eof){
+		line = fin.readln;
+		if(instream == true){
+			if(line == "endstream\n"){
+				instream = false;
+				streams ~= buff;
+				buff = "";
+			}else{
+				buff ~= line;
+			}
+		}else{
+			if(line == "stream\n"){
+				instream = true;
+			}
+		}
+	}
+
+	foreach(i,elem ; streams){
+		auto fout = File("streamOutput/output" ~ to!string(i) ~ ".txt","w");
+		fout.write(to!string(uncompress(elem)));
+	}
+
+
+}
