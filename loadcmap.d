8a9ca97e0fdd2f9683fd5a348cf514494b7a370a
thotgamma <gamma@e.thotgamma.com>
2016-02-12 17:42:07 +0900

    loadcmapできた
    gitignore書き間違えてた☆（ゝω・）vｷｬﾋﾟ

---
+++ loadcmap.d
@@ -0,0 +1,56 @@
+import std.stdio;
+import std.string;
+import std.conv;
+import std.format;
+import std.array;
+
+void main(){
+	string line;
+	string[] sentences;
+	bool loadChar = false;
+	bool loadRange = false;
+
+	uint[uint]cmap;
+
+
+	auto fin = File("resources/cmap/UniJIS2004-UTF8-H.txt","r");
+	while(!fin.eof){
+		line = fin.readln.chomp;
+		if(loadChar == false && loadRange == false){
+			if(indexOf(line,"begincidchar") != -1){
+				loadChar = true;
+			}else if(indexOf(line, "begincidrange") != -1){
+				loadRange = true;
+			}
+		}else if(loadChar == true){
+			if(line == "endcidchar"){
+				loadChar = false;
+				continue;
+			}
+			sentences = split(line," ");
+			string uni = sentences[0][1..$-1];
+			string cid = sentences[1];
+			cmap[to!uint(uni,16)] = to!uint(cid);
+			//writeln(uni ~ " : " ~ cid);
+		}else if(loadRange == true){
+			if(line == "endcidrange"){
+				loadRange = false;
+				continue;
+			}
+			sentences = split(line," ");
+			string rangeStartStr = sentences[0][1..$-1];
+			string rangeEndStr = sentences[1][1..$-1];
+			uint rangeStart = parse!uint(rangeStartStr,16);
+			uint rangeEnd = parse!uint(rangeEndStr,16);
+			uint cidStart = parse!uint(sentences[2]);
+			for(; rangeStart <= rangeEnd; rangeStart++){
+				cmap[rangeStart] = cidStart;
+				//auto writer = appender!string();
+				//formattedWrite(writer,"%x",rangeStart);
+				//writeln(writer.data ~ " : " ~ to!string(cidStart));
+				cidStart ++;
+			}
+		}
+	}
+	writeln(cmap);
+}
