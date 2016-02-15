4725fe41d8bc3b5cf8f72d22489acd53ca90d57f
thotgamma <gamma@e.thotgamma.com>
2016-02-15 18:31:33 +0900

    方向性を変えた

--- fontanalyzer.d
+++ fontanalyzer.d
@@ -2,41 +2,13 @@ import std.stdio;
 import std.conv;
 
 void main(){
+	writeln(trim(0,4));
+}
 
+string trim(int from,int length){
 	auto fin = File("resources/fonts/KozGoPr6N-Medium.otf","rb");
-
-	char headerID[4] = new char[4];
-	fin.rawRead(headerID);
-	writeln("ファイル識別子: " ~ to!string(headerID));
-
-	ushort numOfTable[1] = new ushort[1];
-	fin.rawRead(numOfTable);
-	writeln("テーブルの数: " ~ to!string(numOfTable));
-	writeln(numOfTable.length);
-	foreach(b;numOfTable){
-		writef("%02x",b);
-	}
-	writeln("");
-
-	fin.seek(12);
-	char tableName[4] = new char[4];
-	fin.rawRead(tableName);
-	writeln("テーブルの名前: " ~ to!string(tableName));
-	
-	uint tableInfo[3] = new uint[3];
-	fin.rawRead(tableInfo);
-	writeln("チェックサム: " ~ to!string(tableInfo[0]));
-	writef("%02x",tableInfo[0]);
-	writeln("オフセット: " ~ to!string(tableInfo[1]));
-	writeln("データ長: " ~ to!string(tableInfo[2]));
-
-
-	/*
-	foreach (b;data)
-		writef("%02x ",b);
-	writeln("");
-	*/
-
-
-
+	ubyte buffer[] = new ubyte[length];
+	fin.seek(from);
+	fin.rawRead(buffer);
+	return to!string(buffer);
 }
