e8e7f67caea39714b2fdc13f27856dff50b24623
thotgamma <gamma@e.thotgamma.com>
2016-02-16 20:38:01 +0900

    バイナリ解析用の基礎関数を作った
    手がネギ塩臭せぇ

--- fontanalyzer.d
+++ fontanalyzer.d
@@ -1,14 +1,35 @@
 import std.stdio;
 import std.conv;
+import std.format;
+import std.array;
 
 void main(){
-	writeln(trim(0,4));
+	ubyte[4] a = trim(0,4);
+	writeln(array2uint(a));
+	writeln(array2string(a));
 }
 
-string trim(int from,int length){
+ubyte[] trim(int from,int length){
 	auto fin = File("resources/fonts/KozGoPr6N-Medium.otf","rb");
 	ubyte buffer[] = new ubyte[length];
 	fin.seek(from);
 	fin.rawRead(buffer);
-	return to!string(buffer);
+	return buffer;
+}
+
+uint array2uint(ubyte[] in0){
+	auto writer = appender!string();
+	foreach(elem;in0){
+		formattedWrite(writer,"%x",elem);
+	}
+	return to!uint(writer.data,16);
+}
+
+string array2string(ubyte[] in0){
+	string output;
+	foreach(elem;in0){
+		char buf = elem;
+		output ~= to!string(buf);
+	}
+	return output;
 }
