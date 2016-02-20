b74ad47d1a26937c16d89c7e82fd23134c9db27c
thotgamma <gamma@e.thotgamma.com>
2016-02-20 15:13:30 +0900

    headテーブルを読むようにした
    符号つきキャストができてない

--- fontanalyzer.d
+++ fontanalyzer.d
@@ -18,34 +18,53 @@ void main(){
 		writeln("\tデータ長: " ~ to!string(dataLength));
 		
 		switch(tag){
+			case "head":
+				writeln("\t#version: " ~			to!string(array2uint(trim(offset,4))));
+				writeln("\t#fontRevision: " ~		to!string(array2uint(trim(offset+4,4))));
+				writeln("\tcheckSumAdjustment: " ~	to!string(array2uint(trim(offset+8,4))));
+				writeln("\t#magicNumber: " ~		to!string(array2uint(trim(offset+12,4))));
+				writeln("\t#flags: " ~				to!string(array2uint(trim(offset+16,2))));
+				writeln("\t#unitsPerEm: " ~			to!string(array2uint(trim(offset+18,2))));
+				writeln("\t#created: " ~			to!string(array2ulong(trim(offset+20,8))));
+				writeln("\t#modified: " ~			to!string(array2ulong(trim(offset+28,8))));
+				writeln("\t#xMin: " ~				to!string(array2short(trim(offset+36,2))));
+				writeln("\t#yMin: " ~				to!string(array2short(trim(offset+38,2))));
+				writeln("\t#xMax: " ~				to!string(array2short(trim(offset+40,2))));
+				writeln("\t#yMax: " ~				to!string(array2short(trim(offset+42,2))));
+				writeln("\t#macStyle: " ~			to!string(array2uint(trim(offset+44,2))));
+				writeln("\t#lowestRecPPEM: " ~		to!string(array2uint(trim(offset+46,2))));
+				writeln("\t#fontDirectionHint: " ~	to!string(array2short(trim(offset+48,2))));
+				writeln("\t#indexToLocFormat: " ~	to!string(array2short(trim(offset+50,2))));
+				writeln("\t#glyphDataFormat: " ~	to!string(array2short(trim(offset+52,2))));
+				break;
 			case "cmap":
-				writeln("\t#version: " ~ to!string(array2uint(trim(offset,2))));
-				uint numTables = array2uint(trim(offset+2,2));
+				writeln("\t#version: " ~			to!string(array2uint(trim(offset,2))));
+				uint numTables =							  array2uint(trim(offset+2,2));
 				writeln("\t#numTables: " ~ to!string(numTables));
 				for(int j; j<numTables; j++){
 					writeln("\t-=-=-=-=-=-=-=-=-=[#" ~ to!string(j) ~ "]");
-					writeln("\t##platformID: " ~ to!string(array2uint(trim(offset+4 +8*j,2))));
-					writeln("\t##encodingID: " ~ to!string(array2uint(trim(offset+6 +8*j,2))));
-					uint tableOffset = array2uint(trim(offset+8 +8*j,4));
+					writeln("\t##platformID: " ~	to!string(array2uint(trim(offset+4 +8*j,2))));
+					writeln("\t##encodingID: " ~	to!string(array2uint(trim(offset+6 +8*j,2))));
+					uint tableOffset =						  array2uint(trim(offset+8 +8*j,4));
 					writeln("\t##offset: " ~ to!string(tableOffset));
-					writeln("\t\t#format: " ~ to!string(array2uint(trim(offset + tableOffset,2))));
+					writeln("\t\t#format: " ~		to!string(array2uint(trim(offset + tableOffset,2))));
 				}
 				
 				break;
 			case "hhea":
-				writeln("\t#version: " ~ to!string(array2uint(trim(offset,4))));
-				writeln("\t#Ascender: " ~ to!string(array2uint(trim(offset+4,2))));
-				writeln("\t#Descender: " ~ to!string(array2uint(trim(offset+6,2))));
-				writeln("\t#LineGap: " ~ to!string(array2uint(trim(offset+8,2))));
-				writeln("\t#advanceWidthMax: " ~ to!string(array2uint(trim(offset+10,2))));
+				writeln("\t#version: " ~			to!string(array2uint(trim(offset,4))));
+				writeln("\t#Ascender: " ~			to!string(array2uint(trim(offset+4,2))));
+				writeln("\t#Descender: " ~			to!string(array2uint(trim(offset+6,2))));
+				writeln("\t#LineGap: " ~			to!string(array2uint(trim(offset+8,2))));
+				writeln("\t#advanceWidthMax: " ~	to!string(array2uint(trim(offset+10,2))));
 				writeln("\t#minLeftSideBearing: " ~ to!string(array2uint(trim(offset+12,2))));
-				writeln("\t#minRightSideBearing: " ~ to!string(array2uint(trim(offset+14,2))));
-				writeln("\t#xMaxExtent: " ~ to!string(array2uint(trim(offset+16,2))));
-				writeln("\t#caretSlopeRise: " ~ to!string(array2uint(trim(offset+18,2))));
-				writeln("\t#caretSlopeRun: " ~ to!string(array2uint(trim(offset+20,2))));
-				writeln("\t#caretOffset: " ~ to!string(array2uint(trim(offset+22,2))));
-				writeln("\t#metricDataFormat: " ~ to!string(array2uint(trim(offset+32,2))));
-				writeln("\t#numberOfHMetrics: " ~ to!string(array2uint(trim(offset+34,2))));
+				writeln("\t#minRightSideBearing: "~ to!string(array2uint(trim(offset+14,2))));
+				writeln("\t#xMaxExtent: " ~			to!string(array2uint(trim(offset+16,2))));
+				writeln("\t#caretSlopeRise: " ~		to!string(array2uint(trim(offset+18,2))));
+				writeln("\t#caretSlopeRun: " ~		to!string(array2uint(trim(offset+20,2))));
+				writeln("\t#caretOffset: " ~		to!string(array2uint(trim(offset+22,2))));
+				writeln("\t#metricDataFormat: " ~	to!string(array2uint(trim(offset+32,2))));
+				writeln("\t#numberOfHMetrics: " ~	to!string(array2uint(trim(offset+34,2))));
 				break;
 			default:
 				break;
@@ -62,12 +81,29 @@ ubyte[] trim(int from,int length){
 	return buffer;
 }
 
+short array2short(ubyte[] in0){
+	auto writer = appender!string();
+	foreach(elem;in0){
+		formattedWrite(writer,"%02x",elem);
+	}
+	writeln(writer.data);
+	return to!short(writer.data,16);
+}
+
 uint array2uint(ubyte[] in0){
 	auto writer = appender!string();
 	foreach(elem;in0){
-		formattedWrite(writer,"%x",elem);
+		formattedWrite(writer,"%02x",elem);
 	}
 	return to!uint(writer.data,16);
+}
+
+ulong array2ulong(ubyte[] in0){
+	auto writer = appender!string();
+	foreach(elem;in0){
+		formattedWrite(writer,"%02x",elem);
+	}
+	return to!ulong(writer.data,16);
 }
 
 string array2string(ubyte[] in0){
