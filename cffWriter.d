e2cf2f4e2d0f0a9d288fd0b38fa626dc0ff27b8b
thotgamma <gamma@e.thotgamma.com>
2016-03-02 23:46:28 +0900

    CFFわからん....
    とりまHEADとNAMEINDEXだけ書くようにした

--- cffWriter.d
+++ cffWriter.d
@@ -1,5 +1,29 @@
 import std.stdio;
+import std.conv;
+import std.array;
+import std.format;
+
+ubyte[] binary;
+
+string fontName = "hogeFont";
+auto writer = appender!string();
 
 void main(){
-	writeln("Hello,world!");
+	//HEAD
+	binary ~= [01,00,04,02];
+	//NAME INDEX
+	binary ~= [00,01,01,01];
+	binary ~= fontName.length & 0b11111111;
+	foreach(chr;fontName){
+		writer = appender!string();
+		formattedWrite(writer,"%02x",chr);
+		binary ~= to!ubyte(writer.data,16);
+	}
+	//TOP DICT INDEX
+
+	//STRING INDEX
+	//GLOBAL SUBR INDEX
+
+	writeln(binary);
+
 }
